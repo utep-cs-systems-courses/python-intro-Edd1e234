1735e0030ca92edfd291186747c7f3b0227d998d
Eddie Garcia <edd1e234g@gmail.com>
2020-09-02 10:52:46 -0600

    Cleanup done. Testing needed

--- wordCount.py
+++ wordCount.py
@@ -5,31 +5,35 @@ import os
 
 
 def check_if_files_are_present():
-    if len(sys.argv) is not 2:
-        print("Correct usage: wordCount.py <input text file>")
+    if len(sys.argv) is not 3:
+        print("Correct usage: wordCount.py <input text file> " \
+            "<output text file>")
 
-    textFname = sys.argv[1]
+    input_text_file = sys.argv[1]
+    output_text_file = sys.argv[2]
 
-    if not os.path.exists(textFname):
-        print ("text file input %s doesn't exist! Exiting" % textFname)
+    if not os.path.exists(input_text_file):
+        print ("text file input %s doesn't exist! Exiting" % input_text_file)
+        exit()
+    if not os.path.exists(output_text_file):
+        print ("text file output %s doesn't exist! Exiting" % output_text_file)
         exit()
 
-    return textFname
+    return input_text_file, output_text_file
 
-def open_file_and_count_words():
-    textFname = check_if_files_are_present()
-    open_text_file = open(textFname, "r")
-    lines = open_text_file.read().split("\n")
-
+def open_file_and_count_words(input_text_file):
     dic = {}
 
-    # Spliting by line, then by sentence, then by comma, then finally by space.
-    for line in lines:
+    # Open and split by line.
+    for line in open(input_text_file, "r").read().split("\n"):
         sentences = line.split(".")
+
         for sentence in sentences:
             statements = sentence.split(",")
+
             for statement in statements:
                 words = statement.split(" ")
+
                 for word in words:
                     if word in dic:
                         dic[word] = dic.get(word) + 1
@@ -37,17 +41,19 @@ def open_file_and_count_words():
                         dic[word] = 1
     return dic
 
-def write_to_output_file(dic):
-    output_file = open("output.txt", "w")
+def write_to_output_file(dic, output_file):
+    output_file = open(output_file, "w")
     for word in dic.keys():
         output_file.write(word + " " + str(dic[word]) + "\n")
     output_file.close()
 
 
 def main():
-    write_to_output_file(open_file_and_count_words())
+    input_text_file, output_text_file = check_if_files_are_present()
+    write_to_output_file(
+                open_file_and_count_words(input_text_file), output_text_file)
 
     # Read and count file.
-    print("Hello World")
+    print("Success!")
 
 main()
